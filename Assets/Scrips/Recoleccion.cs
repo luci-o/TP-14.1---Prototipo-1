c66d44b19a7cd0bb63f9650125955d31ddcb8c32
luci-o <luchinototal@gmail.com>
2026-05-27 14:02:01 -0300

    lucio

---
+++ Assets/Scrips/Recoleccion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoleccion : MonoBehaviour
+{
+    // Variable para el contador
+    private int objetosRecolectados = 0;
+
+    // Se activa cuando la esfera toca al reloj
+    private void OnTriggerEnter(Collider other)
+    {
+        // Si lo que tocamos tiene el Tag "Coleccionable"
+        if (other.CompareTag("Coleccionable"))
+        {
+            // Sumamos punto
+            objetosRecolectados++;
+
+            // Mostramos en consola
+            Debug.Log("Objetos recolectados: " + objetosRecolectados);
+
+            // Destruimos el reloj
+            Destroy(other.gameObject);
+        }
+    }
+}
